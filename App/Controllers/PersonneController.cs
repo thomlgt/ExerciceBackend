e99eb319e2891ed4786a9e39a92d51fe1f6b5a53
Thomas.L <thomlgt@hotmail.fr>
2020-03-19 09:22:49 +0100

    Ajout PersonneDTO et PersonneController

---
+++ App/Controllers/PersonneController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+    [Route("api/personne")]
+    [ApiController]
+    public class PersonneController : ControllerBase
+    {
+        public static List<Personne> personnes = new List<Personne>();
+
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<Personne> FindAll()
+        {
+            List<Personne> result = new List<Personne>();
+            foreach(var personne in personnes)
+            {
+                if(personne != null)
+                {
+                    result.Add(personne);
+                }
+            }
+            return result;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Personne FindById(int id)
+        {
+            return personnes[id];
+        }
+    }
+}
---
+++ App/DTO/Personne.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.DTO
+{
+    public class Personne
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public int Age { get; set; }
+
+        public Personne() { }
+
+        public Personne(int id, string nom, string prenom, int age)
+        {
+            Id = id;
+            Nom = nom;
+            Prenom = prenom;
+            Age = age;
+        }
+    }
+}
